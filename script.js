5c761fc88fe2c16649c53a980303d7846119d382
cwilliams0926 <coopwilliams03@hotmail.com>
2025-04-08 12:11:44 -0400

    Add logic to create n^2 divs

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function makeGrid(size) {
     // for loop up to grid size that makes divs
-    // and appends them to the container div
+    // and makes them part of class "square"
+    const container = document.querySelector(".container");
+    for(let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.textContent = "test";
+        container.appendChild(square);
+    }
+
+    // append the divs to container div
 }
 
 makeGrid(16);
