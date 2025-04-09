45a608e06ba9a157f507d2e2ee57c8a4517b8571
cwilliams0926 <coopwilliams03@hotmail.com>
2025-04-09 10:32:45 -0400

    Add new logic to makeGrid function

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ function makeGrid(size) {
     // for loop up to grid size that makes divs
     // and makes them part of class "square"
     const container = document.querySelector(".container");
-    for(let i = 0; i < size * size; i++) {
-        const square = document.createElement("div");
-        container.appendChild(square);
+    for(let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for(let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
     }
 
     // append the divs to container div
